079624a086587b16567dc980ea236a7059356c3e
grabania <grabania.work@gmail.com>
2020-05-10 23:44:14 +0100

    Create question3

--- JavaScript-quiz.js
+++ JavaScript-quiz.js
@@ -22,3 +22,22 @@ function question2(array1) {
 }
 
 console.log(question2(array1));
+
+/* Question 3
+ *
+ * Write a function which takes two arguments, with the second argument having a
+ * default value of false.
+ *
+ * The function should return true if BOTH the first argument AND the second argument are
+ * true, otherwise, it should return false.
+ *
+ * You will also have to edit the parameter list for this function.
+ */
+
+part2 = false;
+part1 = true;
+function question3(part1, part2) {
+	return part1 === true && part2 === true ? true : false;
+}
+
+console.log(question3(part1, part2));
