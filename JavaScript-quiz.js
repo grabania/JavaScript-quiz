079e28f5845178a04c8fcf1f82d6157ae4f541cb
grabania <grabania.work@gmail.com>
2020-05-10 23:35:38 +0100

    Create question1

---
+++ JavaScript-quiz.js
@@ -0,0 +1,12 @@
+/*/ Welcome to the JS quiz!
+
+/* Question 1
+ *
+ * Write a function that takes two numbers and returns the largest number of the two.
+ */
+
+function question1(num1, num2) {
+	return num1 > num2 ? num1 : num2;
+}
+
+console.log(question1(888, 289989));
