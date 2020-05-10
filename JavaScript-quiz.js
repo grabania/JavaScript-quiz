e7442c6a5d41574b3be42bf11e4b64b190575b14
grabania <grabania.work@gmail.com>
2020-05-10 23:38:49 +0100

    Create question2

--- JavaScript-quiz.js
+++ JavaScript-quiz.js
@@ -10,3 +10,15 @@ function question1(num1, num2) {
 }
 
 console.log(question1(888, 289989));
+
+/* Question 2
+ *
+ * Write a function that takes an array and returns the last 3 elements in the array.
+ */
+array1 = [ 1, 2, 3, 4, 5, 6, 89, 9 ];
+
+function question2(array1) {
+	return array1.slice(-3);
+}
+
+console.log(question2(array1));
